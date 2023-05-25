32c2a3b7ddc996c537bdc515f7e7780b3ecf70a4
Fiipe Ferreira <filipemarquesf.dev@gmail.com>
2023-05-25 12:56:00 -0300

    Adicionando icon

--- forge.config.js
+++ forge.config.js
@@ -9,6 +9,17 @@ module.exports = {
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
+      config: {
+        icon: './resources/icon2.icns',
+        darwin: {
+          icon: './resources/icon.icns',
+          category: 'public.app-category.developer-tools',
+          target: [
+            'dmg',
+            'zip',
+          ],
+        },
+      },
     },
     {
       name: '@electron-forge/maker-deb',
