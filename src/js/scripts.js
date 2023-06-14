b5798c94ca859ee125151fe9bf769b6a136fb22f
lipemf <filipemarquesf.dev@gmail.com>
2023-06-14 01:58:19 -0300

    Adicionando rateio por valor

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -73,11 +73,16 @@ function readExcelFileFlat(sheetName) {
     Swal.fire({
       title: "Qual o valor que deve ser feito o rateio?",
       text: "Escreva qual deve ser o valor do rateio:",
-      input: 'number',
+      input: 'text',
+      inputAttributes: {
+        step: '0.01'
+      },
       html: dateTables,
       showCancelButton: true,
     }).then(async (result) => {
-      if (result.value && !isNaN(Date.parse(document.getElementById('startDate').value)) && !isNaN(Date.parse(document.getElementById('endDate').value))) {
+      if (result.value && /^[0-9,]+$/.test(result.value) && !isNaN(Date.parse(document.getElementById('startDate').value)) && !isNaN(Date.parse(document.getElementById('endDate').value))) {
+        
+        
         const startDate = document.getElementById('startDate').value;
 
         // Format the startDate as dd/mm/yyyy
@@ -139,11 +144,11 @@ function readExcelFileFlat(sheetName) {
 
         const sum = Object.values(codCcustoCounts).reduce((acc, curr) => acc + curr, 0);
 
-        // Divide result.value by the total count for each Cód Ccusto
+        // Divide result.value.replace(/,/g, '.') by the total count for each Cód Ccusto
         const dividedResults = {};
         for (const codCcusto in codCcustoCounts) {
           if (codCcustoCounts.hasOwnProperty(codCcusto)) {
-            dividedResults[codCcusto] = result.value / Object.keys(codCcustoCounts).length;
+            dividedResults[codCcusto] = result.value.replace(/,/g, '.') / Object.keys(codCcustoCounts).length;
           }
         }
         
@@ -157,7 +162,7 @@ function readExcelFileFlat(sheetName) {
               <td>${codCcusto}</td>
               <td>${descricaoCcusto}</td>
               <td>${count}</td>
-              <td>${dividedValue.toFixed(2)}</td>
+              <td>${dividedValue.toFixed(2).replace('.',',')}</td>
             </tr>
           `;
         });
@@ -167,7 +172,7 @@ function readExcelFileFlat(sheetName) {
             <i style='font-size: 49px; color: crimson; cursor: pointer' class='bx bx-x' onclick="this.parentElement.remove()"></i>
 
             <h4 style='font-weight: 900'>Tipo do Rateio: Flat</h4>
-            <h4 style='font-weight: 900'>Valor do Rateio: ${result.value}</h4>
+            <h4 style='font-weight: 900'>Valor do Rateio: ${result.value.replace('.', /,/g)}</h4>
             <h4 style='font-weight: 900'>Data de ínicio: ${formattedStartDate}</h4>
             <h4 style='font-weight: 900'>Data final: ${formattedEndDate}</h4>
             <h4 style='font-weight: 900'>Head Count Total: ${sum}</h4>
@@ -260,11 +265,14 @@ function readExcelFilePerson(sheetName) {
     Swal.fire({
       title: "Qual o valor que deve ser feito o rateio?",
       text: "Escreva qual deve ser o valor do rateio:",
-      input: 'number',
+      input: 'text',
+      inputAttributes: {
+        step: '0.01'
+      },
       html: dateTables,
       showCancelButton: true,
     }).then(async (result) => {
-      if (result.value && !isNaN(Date.parse(document.getElementById('startDate').value)) && !isNaN(Date.parse(document.getElementById('endDate').value))) {
+      if (result.value && /^[0-9,]+$/.test(result.value) && !isNaN(Date.parse(document.getElementById('startDate').value)) && !isNaN(Date.parse(document.getElementById('endDate').value))) {
         const startDate = document.getElementById('startDate').value;
 
         // Format the startDate as dd/mm/yyyy
@@ -327,11 +335,11 @@ function readExcelFilePerson(sheetName) {
 
         const totalCount = Object.values(codCcustoCounts).reduce((acc, count) => acc + count, 0);
 
-        // Divide result.value by the total count for each Cód Ccusto
+        // Divide result.value.replace(/,/g, '.') by the total count for each Cód Ccusto
         const dividedResults = {};
         for (const codCcusto in codCcustoCounts) {
           if (codCcustoCounts.hasOwnProperty(codCcusto)) {
-            dividedResults[codCcusto] = result.value / totalCount * codCcustoCounts[codCcusto];
+            dividedResults[codCcusto] = result.value.replace(/,/g, '.') / totalCount * codCcustoCounts[codCcusto];
           }
         }
 
@@ -354,7 +362,7 @@ function readExcelFilePerson(sheetName) {
             <i style='font-size: 49px; color: crimson; cursor: pointer' class='bx bx-x' onclick="this.parentElement.remove()"></i>
 
             <h4 style='font-weight: 900'>Tipo do Rateio: Pessoa</h4>
-            <h4 style='font-weight: 900'>Valor do Rateio: ${result.value}</h4>
+            <h4 style='font-weight: 900'>Valor do Rateio: ${result.value.replace('.', /,/g)}</h4>
             <h4 style='font-weight: 900'>Data de ínicio: ${formattedStartDate}</h4>
             <h4 style='font-weight: 900'>Data final: ${formattedEndDate}</h4>
             <h4 style='font-weight: 900'>Head Count Total: ${sum}</h4>
@@ -400,6 +408,193 @@ function readExcelFilePerson(sheetName) {
 }
 
 
+function readExcelFileByValue(sheetName) {
+  const file = dialog.showOpenDialogSync({
+    properties: ['openFile'],
+    filters: [
+      { name: 'Excel Files', extensions: ['xlsx', 'xls'] },
+      { name: 'All Files', extensions: ['*'] }
+    ]
+  });
+
+  if (!file) {
+    console.log('No file selected.');
+    return;
+  }
+
+  const workSheets = xlsx.parse(file[0]);
+  const sheet = workSheets.find(sheet => sheet.name === sheetName);
+
+  if (sheet) {
+    const data = sheet.data;
+
+    // Get column indices
+    const nomeIndex = data[0].indexOf('Nome');
+    const admissaoIndex = data[0].indexOf('Admissão');
+    const codCcustoIndex = data[0].indexOf('Cód Ccusto');
+    const descricaoCcustoIndex = data[0].indexOf('Descrição Ccusto');
+    const situacaoIndex = data[0].indexOf('Situação');
+    const dataDemissaoIndex = data[0].indexOf('Data Demissão');
+    const tipoEmpregadoIndex = data[0].indexOf('Tipo Empregado');
+
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // getMonth() returns 0-indexed month
+    const lastDay = new Date(year, month, 0).getDate();
+
+    const defaultDate = `${year}-${month.toString().padStart(2, '0')}-${lastDay.toString().padStart(2, '0')}`;
+
+    let dateTables = `
+      <div style="display: inline-grid;">
+        Data de início: <input type="date" style="margin: 15px" id="startDate" name="date" value="${new Date().toISOString().slice(0, 8)}01">
+        Data final: <input type="date" style="margin: 15px" id="endDate" name="date" value="${defaultDate}">
+      </div>
+    `;
+
+    Swal.fire({
+      title: "Qual o valor que deve ser feito o rateio?",
+      text: "Escreva qual deve ser o valor do rateio:",
+      input: 'text',
+      inputAttributes: {
+        step: '0.01'
+      },
+      html: dateTables,
+      showCancelButton: true,
+    }).then(async (result) => {
+      if (result.value && /^[0-9,]+$/.test(result.value) && !isNaN(Date.parse(document.getElementById('startDate').value)) && !isNaN(Date.parse(document.getElementById('endDate').value))) {
+        const startDate = document.getElementById('startDate').value;
+
+        // Format the startDate as dd/mm/yyyy
+        const startDateParts = startDate.split('-');
+        const formattedStartDate = `${startDateParts[2]}/${startDateParts[1]}/${startDateParts[0]}`;
+
+
+        const endDate = document.getElementById('endDate').value;
+
+        // Format the startDate as dd/mm/yyyy
+        const endDateParts = endDate.split('-');
+        const formattedEndDate = `${endDateParts[2]}/${endDateParts[1]}/${endDateParts[0]}`;
+
+        console.log(formattedStartDate, formattedEndDate);
+
+        function isAfter(firstDate, secondDate){
+          const [day1, month1, year1] = firstDate.split('/');
+          const [day2, month2, year2] = secondDate.split('/');
+
+          const date1 = new Date(year1, month1-1, day1);
+          const date2 = new Date(year2, month2-1, day2);
+
+          console.log(date1, " < ",date2);
+          console.log(date1<date2)
+
+          return date1 < date2;
+        }
+
+        // // Filter and process the data
+        const filteredData = data.slice(1).filter((row) => {
+          let situacao = row[situacaoIndex];
+          let dataDemissao = row[dataDemissaoIndex];
+          let admissao = row[admissaoIndex];
+
+          if (
+            (situacao === 'Trabalhando' && isAfter(admissao, formattedEndDate)) ||
+            (situacao === 'Demitido' && isAfter(formattedStartDate, dataDemissao))
+          ) {
+            console.log(row)
+            return true;
+          } else {
+            return false;
+          }
+        });
+          
+        console.log(filteredData)
+          
+
+        // Count rows for each unique value in Cód Ccusto column
+        const codCcustoCounts = {};
+        for (const row of filteredData) {
+          const codCcusto = row[codCcustoIndex];
+          if (codCcusto) {
+            codCcustoCounts[codCcusto] = (codCcustoCounts[codCcusto] || 0) + 1;
+          }
+        }
+
+        console.log('Cód Ccusto Counts:', codCcustoCounts);
+        const sum = Object.values(codCcustoCounts).reduce((acc, curr) => acc + curr, 0);
+
+        const totalCount = Object.values(codCcustoCounts).reduce((acc, count) => acc + count, 0);
+
+        // Divide result.value.replace(/,/g, '.') by the total count for each Cód Ccusto
+        const dividedResults = {};
+        for (const codCcusto in codCcustoCounts) {
+          if (codCcustoCounts.hasOwnProperty(codCcusto)) {
+            dividedResults[codCcusto] = result.value.replace(/,/g, '.') * codCcustoCounts[codCcusto];
+          }
+        }
+
+        const tableRows = Object.entries(codCcustoCounts).map(([codCcusto, count]) => {
+          const descricaoCcusto = data.find(row => row[codCcustoIndex] === codCcusto)[descricaoCcustoIndex];
+          const dividedValue = dividedResults[codCcusto];
+
+          return `
+            <tr>
+              <td>${codCcusto}</td>
+              <td>${descricaoCcusto}</td>
+              <td>${count}</td>
+              <td>${dividedValue.toFixed(2).replace('.',',')}</td>
+            </tr>
+          `;
+        });
+
+        const tableHtml = `
+          <div style='display: inline-grid; margin: 30px; min-width: 500px'>
+            <i style='font-size: 49px; color: crimson; cursor: pointer' class='bx bx-x' onclick="this.parentElement.remove()"></i>
+
+            <h4 style='font-weight: 900'>Tipo do Rateio: Por Valor</h4>
+            <h4 style='font-weight: 900'>Valor do Rateio: ${result.value.replace('.', /,/g)}</h4>
+            <h4 style='font-weight: 900'>Data de ínicio: ${formattedStartDate}</h4>
+            <h4 style='font-weight: 900'>Data final: ${formattedEndDate}</h4>
+            <h4 style='font-weight: 900'>Head Count Total: ${sum}</h4>
+
+            <table style="border-collapse: collapse; width: auto;">
+              <thead style="background-color: #ff0077; color: white;">
+                <tr>
+                  <th style="padding: 8px; text-align: center;">Cód Ccusto</th>
+                  <th style="padding: 8px; text-align: center;">Descrição Ccusto</th>
+                  <th style="padding: 8px; text-align: center;">Head Count</th>
+                  <th style="padding: 8px; text-align: center;">Rateio</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${tableRows.join('')}
+              </tbody>
+            </table>
+            <button style='background-color: #7EAB8E; color: white; margin-top: 30px; font-weight: 700' onclick="exportTableToExcel(this)"> Export as XLSX <i style='font-size: 49px' class='bx bx-file-export' ></i> </button>
+          </div>
+        `;
+
+        const tablesDiv = document.getElementById('table-divs');
+        tablesDiv.innerHTML += tableHtml;
+      }
+      else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Algo deu errado! Verifique o todos os valores inseridos são válidos e tente novamente.',
+          footer: '<a href="../contacts/index.html">Precisa de ajuda?</a>'
+        });
+      }
+    });
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Algo deu errado! Verifique o todos os valores inseridos são válidos e tente novamente.',
+      footer: '<a href="../contacts/index.html">Precisa de ajuda?</a>'
+    });
+    console.log(`Sheet named "${sheetName}" not found.`);
+  }
+}
 
 
 
